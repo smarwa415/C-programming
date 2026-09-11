135bd476b1ace8efba80120bca02190390bee866
smarwa415 <marwasocrates@gmail.com>
2026-09-11 15:50:12 +0300

    initial commit of variable c

---
+++ units_offered.cpp
@@ -0,0 +1,26 @@
+// single line comment
+// program to display units offered this semester
+/*
+multiline comment
+Author:Marwa Socrates Makongo
+Reg NO.: BCS-05-0047/2025
+Date : 12/9/2026
+Description: units offered this semester program
+Version: 2
+*/
+#include<stdio.h> // scanf();printf()
+int main(){
+	printf("Units offered this semester:\n");
+printf("1.Structured programming\n");
+printf("2.Communication skils\n");
+printf("3.Mathematical physics\n");
+printf("4.Health and wellness management\n");
+printf("5.Introduction to computer and internet\n");
+printf("6.Basic athematics\n");
+printf("7.Computational logic\n");
+return 0; //execution successful
+
+}
+
+
+
